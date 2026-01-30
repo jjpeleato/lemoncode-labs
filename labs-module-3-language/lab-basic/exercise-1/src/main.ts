739aea3e9126008f6667b6b72f32991115a7c3f4
José J. Peleato Pradel <jjpeleato@gmail.com>
2026-01-30 18:02:09 +0100

    feat(language): create tail function

--- labs-module-3-language/lab-basic/exercise-1/src/main.ts
+++ labs-module-3-language/lab-basic/exercise-1/src/main.ts
@@ -1,4 +1,4 @@
-import { head } from "./functions";
+import { head, tail } from "./functions";
 
 console.log("########## Module 3 - Basic Laboratory - Exercise 1 - Array operations ##########");
 
@@ -9,3 +9,11 @@ console.log(head(['a', 'b', 'c', 'd']));				// Return 'a'
 console.log(head([]));									// Return undefined
 
 console.log("########## END: Head function ##########");
+
+console.log("########## START: Tail function ##########");
+
+console.log(tail([0, 1, 2, 3]));						// Return [1, 2, 3]
+console.log(tail(['a', 'b', 'c', 'd']));				// Return ['b', 'c', 'd']
+console.log(tail([]));									// Return undefined
+
+console.log("########## END: Tail function ##########");
