1e4e9c5ed62b570405e83ca52f2af656a7c885f5
José J. Peleato Pradel <jjpeleato@gmail.com>
2026-02-11 07:27:29 +0100

    feat(language): install basic structure of exercise 5

---
+++ labs-module-3-language/lab-basic/exercise-5/src/main.ts
@@ -0,0 +1,5 @@
+console.log("########## Module 3 - Basic Laboratory - Exercise 5 - Slot machine ##########");
+
+console.log("########## START: Read book function ##########");
+
+console.log("########## END: Read book function ##########");
