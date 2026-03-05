ea00ce545872bda699668cb9beb839c6815fe58e
José J. Peleato Pradel <jjpeleato@gmail.com>
2026-03-05 12:00:34 +0100

    feat(bundling): install and implement visualizer and compression bundles

--- labs-module-4-bundling/lab-vite/vite.config.ts
+++ labs-module-4-bundling/lab-vite/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import checker from "vite-plugin-checker";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
     checker({
       typescript: true,
+    }),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+    visualizer({
+      filename: "stats.html",
+      open: true,
     }),
   ],
 });
