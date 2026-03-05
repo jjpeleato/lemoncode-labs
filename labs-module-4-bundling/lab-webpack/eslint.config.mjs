b21398cf8c86cc92149d09e3b3a8e5b734af65e9
José J. Peleato Pradel <jjpeleato@gmail.com>
2026-03-05 11:33:47 +0100

    feat(bundling): implement and configuration vite with typescript

--- labs-module-4-bundling/lab-webpack/eslint.config.mjs
+++ labs-module-4-bundling/lab-webpack/eslint.config.mjs
@@ -6,33 +6,33 @@ import reactHooksPlugin from "eslint-plugin-react-hooks";
 import globals from "globals";
 
 export default [
-	js.configs.recommended,
-	{
-		files: ["**/*.{ts,tsx}"],
-		languageOptions: {
-			parser: typescriptParser,
-			ecmaVersion: "latest",
-			sourceType: "module",
-			globals: {
-				...globals.browser, // Define HTMLElement, window, document...
-				...globals.node, // Define process, __dirname...
-			},
-		},
-		plugins: {
-			"@typescript-eslint": typescriptEslint,
-			react: reactPlugin,
-			"react-hooks": reactHooksPlugin,
-		},
-		rules: {
-			...typescriptEslint.configs.recommended.rules,
-			...reactPlugin.configs.recommended.rules,
-			...reactHooksPlugin.configs.recommended.rules,
-			"react/react-in-jsx-scope": "off",
-		},
-		settings: {
-			react: {
-				version: "detect",
-			},
-		},
-	},
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser, // Define HTMLElement, window, document...
+        ...globals.node, // Define process, __dirname...
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
