1ec58337a7824ebe122494e7fe56fde66e865e69
José J. Peleato Pradel <jjpeleato@gmail.com>
2026-04-25 16:59:34 +0200

    refactor(react): move InfoRow into shared directory

--- labs-module-5-react/lab-basic-rick-morty/exercise-1/src/features/characters/index.ts
+++ labs-module-5-react/lab-basic-rick-morty/exercise-1/src/features/characters/index.ts
@@ -3,6 +3,5 @@ export * from "./components/CharacterDetail/CharacterDetail";
 export * from "./components/CharacterList/CharacterList";
 export * from "./components/CharacterPagination/CharacterPagination";
 export * from "./components/CharacterSearchBar/CharacterSearchBar";
-export * from "./components/InfoRow/InfoRow";
 export * from "./hooks/useCharacter";
 export * from "./hooks/useCharacters";
--- labs-module-5-react/lab-basic-rick-morty/exercise-1/src/shared/index.ts
+++ labs-module-5-react/lab-basic-rick-morty/exercise-1/src/shared/index.ts
@@ -1,1 +1,2 @@
+export * from "./components/InfoRow/InfoRow";
 export * from "./hooks/useDebounce";
